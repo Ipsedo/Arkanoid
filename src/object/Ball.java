ca85105f6abc67d20dd0963eda1d37615817aa65
Samuel Berrien <sberrie@a-018402.ups.u-psud.fr>
2017-05-04 12:12:42 +0200

    need fix bounding

--- src/object/Ball.java
+++ src/object/Ball.java
@@ -6,11 +6,11 @@ public class Ball extends Item {
 
 	public Ball(Random rand, int xMin, int xMax, int yMin, int yMax) {
 		super(new float[]{xMin + rand.nextFloat() * (xMax - 10) + 10,
-				yMin + rand.nextFloat() * (yMax - 10) + 10},
-				new float[]{rand.nextFloat() / 2f, rand.nextFloat() / 2f},
-				new float[]{0f, 0f},
-				10,
-				10);
+						yMin + rand.nextFloat() * (yMax - 10) + 10},
+						new float[]{rand.nextFloat() / 2f, rand.nextFloat() / 2f},
+						new float[]{0f, 0f},
+						10,
+						10);
 	}
 	
 	public Ball(float[] mPosition, float[] mSpeed, float[] mAcceleration) {
