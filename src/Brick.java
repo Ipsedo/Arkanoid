e7c869b7d68f8d8852d8080c2e356d3a83e89f28
Samuel Berrien <sberrie@a-018401.ups.u-psud.fr>
2017-05-04 10:11:47 +0200

    Add Brick

---
+++ src/Brick.java
@@ -0,0 +1,16 @@
+
+public class Brick {
+	
+	private float[] mPosition;
+	private float[] mSpeed;
+	private float[] mAcceleration;
+	
+	private int width;
+	private int height;
+	
+	
+	public Brick() {
+		
+	}
+
+}
