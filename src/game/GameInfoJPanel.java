213bd01af17577e3077d037d3537e45afccef9c5
Alexis Marembert <graguelame@gmail.com>
2017-05-05 17:03:01 +0200

    gameInfo boutons changés

--- src/game/GameInfoJPanel.java
+++ src/game/GameInfoJPanel.java
@@ -1,6 +1,8 @@
 package game;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -25,30 +27,35 @@ public class GameInfoJPanel extends JPanel {
 		this.setBackground(new Color(236, 240, 241));
 		
 		this.jframe = f;
+		JButton start = new JButton("Start 'S'");
 		JButton pause = new JButton("Pause 'P'");
 		JButton resume = new JButton("Resume 'O'");
 		JButton reset = new JButton("Reset 'R'");
+		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new GridLayout(5,1,100,0));
+		setLayout(new BorderLayout());
 		
 		String[] levelList = {"Level 0", "Level 1", "Level 3", "Level 4", "Level 5"};
 		JComboBox<String> comboBox = new JComboBox<String>(levelList);
 		comboBox.setEditable(false);
 		comboBox.setBackground(new Color(189, 195, 199));
 		
+		start.setBackground(new Color(189, 195, 199));
 		pause.setBackground(new Color(189, 195, 199));
 		resume.setBackground(new Color(189, 195, 199));
 		reset.setBackground(new Color(189, 195, 199));
 		
-		this.add(comboBox);
-		this.add(new JSeparator(SwingConstants.HORIZONTAL));
+		infoPanel.add(comboBox);
+		this.add(comboBox, BorderLayout.NORTH);
 		
-		this.add(pause);
-		this.add(new JSeparator(SwingConstants.HORIZONTAL));
-		this.add(resume);
-		this.add(new JSeparator(SwingConstants.HORIZONTAL));
-		this.add(reset);
-		this.add(new JSeparator(SwingConstants.HORIZONTAL));
+		infoPanel.add(start);
+		infoPanel.add(pause);
+		infoPanel.add(resume);
+		infoPanel.add(reset);
+		add(infoPanel, BorderLayout.CENTER);
 			
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		//this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
 		this.jframe.addKeyListener(new KeyListener() {
 			@Override
